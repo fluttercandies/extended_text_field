555223ff6cc42b9bc84bb26c8a397a26f54f4f59
zmtzawqlp <zmtzawqlp@live.com>
2019-04-26 18:10:28 +0800

    add image span

--- lib/extended_text_field.dart
+++ lib/extended_text_field.dart
@@ -3,3 +3,4 @@ library extended_text_field;
 export 'src/extended_text_field.dart';
 export 'src/text_span/special_text_span.dart';
 export 'package:extended_text/extended_text.dart';
+export 'src/text_span/text_field_image_span.dart';
