b4f0e150d8fae42cf2d447c26018befcb31935bb
zhoumaotuo <zmtzawqlp@live.com>
2019-05-01 13:31:57 +0800

    update demo

--- example/lib/special_text/at_text.dart
+++ example/lib/special_text/at_text.dart
@@ -4,20 +4,28 @@ import 'package:flutter/material.dart';
 class AtText extends SpecialText {
   static const String flag = "@";
   final int start;
-  AtText(TextStyle textStyle, SpecialTextGestureTapCallback onTap, this.start)
+
+  /// whether show background for @somebody
+  final bool showAtBackground;
+  AtText(TextStyle textStyle, SpecialTextGestureTapCallback onTap, this.start,
+      {this.showAtBackground: false})
       : super(flag, " ", textStyle, onTap: onTap);
 
   @override
   TextSpan finishText() {
     // TODO: implement finishText
+    TextStyle textStyle = showAtBackground
+        ? this.textStyle?.copyWith(
+            fontSize: 16.0,
+            background: Paint()..color = Colors.blue.withOpacity(0.5))
+        : this.textStyle?.copyWith(color: Colors.blue, fontSize: 16.0);
 
     final String atText = toString();
     return SpecialTextSpan(
-      text: atText,
-      actualText: atText,
-      start: start,
-      deleteAll: false,
-      style: textStyle?.copyWith(color: Colors.blue, fontSize: 16.0),
-    );
+        text: atText,
+        actualText: atText,
+        start: start,
+        deleteAll: false,
+        style: textStyle);
   }
 }
