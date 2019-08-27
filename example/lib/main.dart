b3f0c88a73d19519f467ad9d72a9ff8dac0017f6
zmtzawqlp <zmtzawqlp@live.com>
2019-08-27 10:59:33 +0800

    add ff_annotation_route to generate demo route

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,126 +1,101 @@
-import 'package:example/text_demo.dart';
-import 'package:example/widget_span.dart';
+import 'dart:io';
+
+import 'package:example/pages/no_route.dart';
+import 'package:example/pages/text_demo.dart';
+import 'package:example/pages/widget_span.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:oktoast/oktoast.dart';
 
 import 'common/tu_chong_repository.dart';
-import 'custom_toolbar.dart';
+import 'package:example/pages/custom_toolbar.dart';
+
+import 'example_route.dart';
+import 'example_route_helper.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp() {
+    clearDiskCachedImages(duration: Duration(days: 7));
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return OKToast(
         child: MaterialApp(
+      title: 'extended text field demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      navigatorObservers: [
+        FFNavigatorObserver(routeChange: (name) {
+          //you can track page here
+          // print(name);
+        }, showStatusBarChange: (bool showStatusBar) {
+          if (showStatusBar) {
+            SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+          } else {
+            SystemChrome.setEnabledSystemUIOverlays([]);
+          }
+        })
+      ],
+      builder: (c, w) {
+        ScreenUtil.instance =
+            ScreenUtil(width: 750, height: 1334, allowFontScaling: true)
+              ..init(c);
+        var data = MediaQuery.of(c);
+        return MediaQuery(
+          data: data.copyWith(textScaleFactor: 1.0),
+          child: w,
+        );
+      },
+      initialRoute: "fluttercandies://mainpage",
+      onGenerateRoute: (RouteSettings settings) {
+        var routeResult =
+            getRouteResult(name: settings.name, arguments: settings.arguments);
+
+        if (routeResult.showStatusBar != null ||
+            routeResult.routeName != null) {
+          settings = FFRouteSettings(
+              arguments: settings.arguments,
+              name: settings.name,
+              isInitialRoute: settings.isInitialRoute,
+              routeName: routeResult.routeName,
+              showStatusBar: routeResult.showStatusBar);
+        }
+
+        var page = routeResult.widget ?? NoRoute();
+
+        switch (routeResult.pageRouteType) {
+          case PageRouteType.material:
+            return MaterialPageRoute(settings: settings, builder: (c) => page);
+          case PageRouteType.cupertino:
+            return CupertinoPageRoute(settings: settings, builder: (c) => page);
+          case PageRouteType.transparent:
+            return Platform.isIOS
+                ? TransparentCupertinoPageRoute(
+                    settings: settings, builder: (c) => page)
+                : TransparentMaterialPageRoute(
+                    settings: settings, builder: (c) => page);
+//            return FFTransparentPageRoute(
+//                settings: settings,
+//                pageBuilder: (BuildContext context, Animation<double> animation,
+//                        Animation<double> secondaryAnimation) =>
+//                    page);
+          default:
+            return Platform.isIOS
+                ? CupertinoPageRoute(settings: settings, builder: (c) => page)
+                : MaterialPageRoute(settings: settings, builder: (c) => page);
+        }
+      },
     ));
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Page> pages = new List<Page>();
-  TuChongRepository _listSourceRepository = TuChongRepository();
-  @override
-  void initState() {
-    pages.add(Page(
-        PageType.text, "build special text and inline image in text field"));
-    pages.add(Page(PageType.toolbar_hanldes,
-        "custom selection toolbar and handles for text field"));
-    pages.add(Page(PageType.widgetSpan, "mailbox demo with widgetSpan"));
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _listSourceRepository.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var content = ListView.builder(
-      itemBuilder: (_, int index) {
-        var page = pages[index];
-
-        Widget pageWidget;
-        return Container(
-          margin: EdgeInsets.all(20.0),
-          child: GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  (index + 1).toString() +
-                      "." +
-                      page.type.toString().replaceAll("PageType.", ""),
-                  //style: TextStyle(inherit: false),
-                ),
-                Text(
-                  page.description,
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            ),
-            onTap: () {
-              switch (page.type) {
-                case PageType.text:
-                  pageWidget = TextDemo(_listSourceRepository);
-                  break;
-                case PageType.toolbar_hanldes:
-                  pageWidget = CustomToolBar();
-                  break;
-                case PageType.widgetSpan:
-                  pageWidget = WidgetSpanDemo();
-                  break;
-                default:
-                  break;
-              }
-              Navigator.push(context,
-                  new MaterialPageRoute(builder: (BuildContext context) {
-                return pageWidget;
-              }));
-            },
-          ),
-        );
-      },
-      itemCount: pages.length,
-    );
-
-    return Scaffold(
-      body: content,
-    );
-  }
-}
-
-class Page {
-  final PageType type;
-  final String description;
-  Page(this.type, this.description);
-}
-
-enum PageType { text, toolbar_hanldes, widgetSpan }
